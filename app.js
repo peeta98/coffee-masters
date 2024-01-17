dcdfa8af6309eeaaebea46b5d7c52dbc8b850a36
peeta98 <pedromnmoreira98@gmail.com>
2024-01-17 14:04:00 -0100

    Add Router funcionality

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import Store from "./services/Store.js";
 // import API from "./services/API.JS";
 import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
 
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 // It's better to wait for the event for manipulation
 window.addEventListener("DOMContentLoaded", async () => {
   loadData();
+  app.router.init();
 });

1924686e2396d0382e7ba796c8942822d0e9bd6f
peeta98 <pedromnmoreira98@gmail.com>
2024-01-22 15:47:18 -0100

    project finished

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ import Router from "./services/Router.js";
 // link my Web Components
 import { MenuPage } from "./components/MenuPage.js";
 import { DetailsPage } from "./components/DetailsPage.js";
-import { OrderPage } from "./components/Order.js";
+import { OrderPage } from "./components/OrderPage.js";
 import ProductItem from "./components/ProductItem.js";
+import CartItem from "./components/CartItem.js";
 
 window.app = {};
 app.store = Store;
@@ -17,4 +18,11 @@ app.router = Router;
 window.addEventListener("DOMContentLoaded", async () => {
   loadData();
   app.router.init();
+});
+
+window.addEventListener("appcartchange", (event) => {
+  const badge = document.getElementById("badge");
+  const qty = app.store.cart.reduce((acc, item) => acc + item.quantity, 0);
+  badge.textContent = qty;
+  badge.hidden = qty == 0;
 });
--- components/Order.js
+++
@@ -1,7 +0,0 @@
-export class OrderPage extends HTMLElement {
-  constructor() {
-    super();
-  }
-}
-
-customElements.define("order-page", OrderPage);
